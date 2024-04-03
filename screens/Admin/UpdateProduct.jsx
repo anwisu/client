0601cf8fb27eb36f16b7e890b27f6d7bdb04a0de
gigilmoko <gregorijiselle2008@gmail.com>
2024-04-03 09:55:29 +0800

    update product

--- screens/Admin/UpdateProduct.jsx
+++ screens/Admin/UpdateProduct.jsx
@@ -1,14 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-    StyleSheet,
-    Text,
-    Image,
-    StatusBar,
-    View,
-    KeyboardAvoidingView,
-    ScrollView,
-    TouchableOpacity,
-} from "react-native";
+import { View, ScrollView, TouchableOpacity, Text, StyleSheet } from "react-native";
 import { Button, TextInput } from "react-native-paper";
 import { useIsFocused } from "@react-navigation/native";
 import { useDispatch, useSelector } from "react-redux";
@@ -22,14 +13,8 @@ import { getProductDetails } from "../../redux/actions/productActions";
 import { updateProduct } from "../../redux/actions/otherActions";
 import SelectComponent from "../../components/Layout/SelectComponent";
 import { deleteProductImage, updateProductImage } from "../../redux/actions/otherActions";
-import { IconButton } from "react-native-paper";
-import { colors, network } from "../../constants";
-import { Ionicons } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
-import DropDownPicker from "react-native-dropdown-picker";
-import Header from "../../components/Layout/Header";
 
-const UpdateProduct = ({ navigation, route }) => {
+const UpdateProduct= ({ navigation, route }) => {
     const isFocused = useIsFocused();
     const dispatch = useDispatch();
     const [visible, setVisible] = useState(false);
@@ -59,7 +44,7 @@ const UpdateProduct = ({ navigation, route }) => {
         };
         fetchData();
     }, [dispatch, route.params.id, isFocused]);
-
+    
     const submitHandler = async () => {
         setLoading(true);
         try {
@@ -96,7 +81,7 @@ const UpdateProduct = ({ navigation, route }) => {
         }
     }, [product]);
 
-    // Update Images
+// Update Images
     useEffect(() => {
         if (route.params?.image) {
             setSelectedImages([...selectedImages, route.params.image]);
@@ -126,7 +111,7 @@ const UpdateProduct = ({ navigation, route }) => {
         try {
             console.log("Deleting image with ID:", imageId);
             console.log("Image URL:", imageUrl);
-
+    
             if (imageUrl) {
                 console.log("Deleting image from fetched images");
                 await dispatch(deleteProductImage(productId, imageId));
@@ -142,259 +127,148 @@ const UpdateProduct = ({ navigation, route }) => {
             console.error("Error deleting image:", error);
         }
     };
-
-
+    
+    
     const renderCarouselItem = ({ item }) => {
         const imageId = item._id || item.id;
         const imageUrl = item.url || item.uri;
-
+    
         return (
-            <View>
-                <ImageCard src={imageUrl} id={imageId} deleteHandler={deleteHandler} />
+            <View style={styles.carouselItem}>
+                <ImageCard src={imageUrl} id={imageId} deleteHandler={deleteHandler} containerStyle={styles.imageCard} />
             </View>
         );
     };
-
+    
     return (
-        <>
-            <Header back={true} />
-            <KeyboardAvoidingView style={styles.container}>
-                <View style={styles.screenNameContainer}>
-                    <View>
-                        <Text style={styles.screenNameText}>Edit Product</Text>
-                    </View>
-                    <View>
-                        <Text style={styles.screenNameParagraph}>Edit product details</Text>
-                    </View>
-                </View>
-                <ScrollView style={{ flex: 1, width: "100%" }}>
-                    <View style={styles.formContainer}>
-                        <View style={styles.imageContainer}>
-                                <View >
-                                <Carousel
-                                    data={[...(product?.images || []), ...selectedImages]}
-                                    renderItem={renderCarouselItem}
-                                    sliderWidth={300}
-                                    itemWidth={300}
-                                    layout={"default"}
-                                />
-                                </View>
-                        </View>
-                        <Button
-                            mode="contained"
-                            onPress={openImagePicker}
-                            style={{ backgroundColor: "#BC430B", marginHorizontal: 80, marginTop: 10, }}
-                        >
-                            Select Images
-                        </Button>
-                        <View style={styles.inputContainer}>
-                            <View >
-                                <Text style={styles.label}>Name:</Text>
-                                <TextInput
-                                    placeholder="Name"
-                                    value={name}
-                                    onChangeText={setName}
-                                    style={styles.customInput}
-                                />
-                            </View>
-
-                            {/* Description Input */}
-                            <View>
-                                <Text style={styles.label}>Description:</Text>
-                                <TextInput
-                                    placeholder="Description"
-                                    value={description}
-                                    onChangeText={setDescription}
-                                    style={styles.customInput}
-                                />
-                            </View>
-
-                            {/* Price Input */}
-                            <View>
-                                <Text style={styles.label}>Price:</Text>
-                                <TextInput
-                                    placeholder="Price"
-                                    value={price}
-                                    keyboardType="number-pad"
-                                    onChangeText={setPrice}
-                                    style={styles.priceInput}
-                                />
-                            </View>
-
-                            {/* Stock Input */}
-                            <View>
-                                <Text style={styles.label}>Stock:</Text>
-                                <TextInput
-                                    placeholder="Stock"
-                                    value={stock}
-                                    keyboardType="number-pad"
-                                    onChangeText={setStock}
-                                    style={styles.priceInput}
-                                />
-                            </View>
-
-                            {/* Category Text */}
-                            <View>
-                                <Text style={styles.label}>Category:</Text>
-                                <Text
-                                    style={{
-                                        textAlign: "center",
-                                        textAlignVertical: "center",
-                                        borderRadius: 3,
-                                        backgroundColor: "#F9DAA2",
-                                        padding: 5,
-                                        width: 292,
-                                        marginTop: 10,
-                                        textAlign: "left",
-                                        height: 50
-                                    }}
-                                    onPress={() => setVisible(true)}
-                                >
-                                    {category}
-                                </Text>
-                            </View>
-                        </View>
-                        <Button
-                            textColor="#FFFFFF"
-                            style={{                     
-                                backgroundColor: "#BC430B",
-                                padding: 15,
-                                width: "100%",
-                                marginBottom: 10,
-                                alignItems: "center",
-                                borderRadius: 10,
-                                color: "#FFFFFF",
-                            }}
-                            onPress={submitHandler}
-                            loading={loading}
-                            disabled={loading}
-                        >
-                            Update
-                        </Button>
-                    </View>
+        <View style={{ flex: 1, backgroundColor: "#f4b546" }}>
+            <View style={{ flexDirection: "row", justifyContent: "flex-start" }}>
+                <TouchableOpacity
+                    onPress={() => {
+                        if (navigation.canGoBack()) {
+                            navigation.goBack();
+                        } else {
+                            console.log("Can't go back");
+                        }
+                    }}
+                    style={{ backgroundColor: "#bc430b", padding: 10, borderRadius: 5, margin: 10 }}
+                >
+                    <Icons.ArrowLeftIcon size={20} color="white" />
+                </TouchableOpacity>
+            </View>
+            <View style={{ justifyContent: "center", height: 700 }}>
+                <ScrollView>
+                    <Carousel
+                        data={[...(product?.images || []), ...selectedImages]}
+                        renderItem={renderCarouselItem}
+                        sliderWidth={300}
+                        itemWidth={300}
+                        layout={"default"}
+                    />
                 </ScrollView>
-                <SelectComponent
-                    categories={categories}
-                    setCategoryID={setCategoryID}
-                    setCategory={setCategory}
-                    visible={visible}
-                    setVisible={setVisible}
-                />
-
-            </KeyboardAvoidingView>
-        </>
+                <Button
+                    mode="contained"
+                    onPress={openImagePicker}
+                    style={{ marginBottom: 20,  marginHorizontal: 20, padding: 6, backgroundColor: "#BC430B" }}
+                >
+                    Select Images
+                </Button>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Name:</Text>
+                    <TextInput
+                        placeholder="Name"
+                        value={name}
+                        onChangeText={setName}
+                        style={styles.input}
+                    />
+                </View>
+    
+                {/* Description Input */}
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Description:</Text>
+                    <TextInput
+                        placeholder="Description"
+                        value={description}
+                        onChangeText={setDescription}
+                        style={styles.input}
+                    />
+                </View>
+    
+                {/* Price Input */}
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Price:</Text>
+                    <TextInput
+                        placeholder="Price"
+                        value={price}
+                        keyboardType="number-pad"
+                        onChangeText={setPrice}
+                        style={styles.input}
+                    />
+                </View>
+    
+                {/* Stock Input */}
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Stock:</Text>
+                    <TextInput
+                        placeholder="Stock"
+                        value={stock}
+                        keyboardType="number-pad"
+                        onChangeText={setStock}
+                        style={styles.input}
+                    />
+                </View>
+    
+                {/* Category Text */}
+                <View style={styles.categoryContainer}>
+                    <Text style={styles.label}>Category:</Text>
+                    <Text
+                        style={{
+                        textAlign: "center",
+                        textAlignVertical: "center",
+                        borderRadius: 3,
+                        backgroundColor: "#F9DAA2",
+                        padding: 5,
+                        width: 242,
+                        textAlign: "left"
+                    }}
+                    onPress={() => setVisible(true)}
+                    >
+                    {category}
+                </Text>
+                </View> 
+                <Button
+                    textColor="#FFFFFF"
+                    style={{ backgroundColor: "#bc430b", marginHorizontal: 20, padding: 6 }}
+                    onPress={submitHandler}
+                    loading={loading}
+                    disabled={loading}
+                >
+                    Update
+                </Button>
+            </View>  
+            <SelectComponent
+                categories={categories}
+                setCategoryID={setCategoryID}
+                setCategory={setCategory}
+                visible={visible}
+                setVisible={setVisible}
+            />
+        </View>
     );
-
+    
 }
 
 export default UpdateProduct;
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirecion: "row",
-        backgroundColor: colors.light,
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 20,
-        flex: 1,
-    },
-    TopBarContainer: {
-        width: "100%",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        alignItems: "center",
-    },
-    formContainer: {
-        flex: 2,
-        justifyContent: "flex-start",
-        alignItems: "center",
-        display: "flex",
-        width: "100%",
-        flexDirecion: "row",
-        padding: 5,
-    },
-
-    buttomContainer: {
-        marginTop: 10,
-        width: "100%",
-    },
-    bottomContainer: {
-        marginTop: 10,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-    },
-    screenNameContainer: {
-        marginTop: 10,
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        alignItems: "flex-start",
-    },
-    screenNameText: {
-        fontSize: 30,
-        fontWeight: "800",
-        color: colors.muted,
-    },
-    screenNameParagraph: {
-        marginTop: 5,
-        fontSize: 15,
-    },
-    imageContainer: {
-        display: "flex",
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        width: "100%",
-        height: 250,
-        backgroundColor: colors.white,
-        borderRadius: 10,
-        elevation: 5,
-        paddingLeft: 20,
-        paddingRight: 20,
-    },
-    imageHolder: {
-        height: 200,
-        width: 200,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: colors.light,
-        borderRadius: 10,
-        elevation: 5,
-    },
-    customInput: {
-        width: 292,
-        height: 40,
-        marginBottom: 10,
-        marginTop: 10,
-        width: "100%",
-        padding: 5,
-        backgroundColor: colors.white,
-        elevation: 5,
-        paddingHorizontal: 20,
-    },
-    priceInput: {
-        width: 292,
-        height: 40,
-        marginBottom: 10,
-        marginTop: 10,
-        padding: 5,
-        backgroundColor: colors.white,
-        elevation: 5,
-        paddingHorizontal: 20,
-    },
     inputContainer: {
-        flexDirection: 'column',
-        backgroundColor: colors.light,
-        alignItems: 'flex-start',
-        justifyContent: 'flex-start',
-        padding: 20,
-        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginHorizontal: 30,
+        marginBottom: 10,
     },
     label: {
-        // marginRight: 10,
+        marginRight: 10,
         fontWeight: 'bold',
         fontSize: 16,
         width: 100,
@@ -402,7 +276,7 @@ const styles = StyleSheet.create({
     input: {
         flex: 1,
         backgroundColor: 'transparent',
-        height: 40,
+        height: 40, 
     },
     categoryContainer: {
         flexDirection: 'row',
@@ -416,4 +290,13 @@ const styles = StyleSheet.create({
         fontSize: 16,
         width: 100,
     },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    carouselItem: {
+        flex: 1,
+    },
+
 });
